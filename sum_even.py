75536e63ad2e4b3c17840d900755bb71213f62a0
AktamNosirov <100982538+AktamNosirov@users.noreply.github.com>
2022-03-09 17:45:50 +0500

    Add files via upload

--- sum_even.py
+++ sum_even.py
@@ -7,8 +7,12 @@
 #Find the sum of the even digits in the variable "var_int".
 
 x=int(input())
-a=round((x%10)+1)
+a=((x%10)+1)%2
+A=a*(x%10)
 b=((x//10)%10+1)%2
+B=b*(x//10)%10
 c=((x//100)%10+1)%2
+C=c*(x//100)%10
 d=((x//1000)+1)%2
-print(a+b+c+d)
+D=d*(x//1000)
+print(A+B+C+D)
--- sum_odd.py
+++ sum_odd.py
@@ -5,3 +5,16 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+x=int(input())
+a=((x%10))%2
+A=a*(x%10)
+b=((x//10)%10)%2
+B=b*(x//10)%10
+c=((x//100)%10)%2
+C=c*(x//100)%10
+d=((x//1000))%2
+D=d*(x//1000)
+print(A+B+C+D)
+
+
+
